41343c44f2fb36e6b08afe18712c8cd19a1b2b9b
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-02-05 16:02:50 +0100

    tests

--- find-needle.js
+++ find-needle.js
@@ -1,8 +1,6 @@
 const findNeedle = function(words, word) {
     // Schrijf hier de functie...
-        words.indexOf(word);
+        return words.indexOf(word);
   };
   
   module.exports = findNeedle;
-
-  findNeedle(["house", "train", "slide", "needle", "book"],"needle");
